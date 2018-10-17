817207612684b9ebe11207a20838c65f46687d16
Alexandre Belloni Aves <alexbelloni@outlook.com>
2018-10-17 08:24:05 -0400

    CORS config

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const debug = require('debug')('myserver:server');
 
 const app = require('./src/app');
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
